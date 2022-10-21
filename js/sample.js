80afb6c3d682f77cd99f68bc7d650bc8201e985d
oyenet1 <bowofadeoyerinde@gmail.com>
2022-10-21 01:48:19 +0100

    june

---
+++ js/sample.js
@@ -0,0 +1,25 @@
+function sayHi() {
+  console.log("hi hi hi");
+}
+
+function sayMyName(name) {
+  console.log(name);
+}
+
+function douB(n) {
+  console.log(n * 2);
+}
+
+function evenOdd(num) {
+  if (num % 2 == 0) {
+    return "Even number";
+  } else {
+    return "Odd number";
+  }
+}
+
+function strLent(str) {
+  return str.toUpperCase();
+}
+
+console.log(strLent("AdeOlU"));
